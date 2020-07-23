5dd00796498674d4343541a1d982a9d00bd3459d
hayaprils <aprilia.nurkasanah@students.amikom.ac.id>
2020-07-23 19:41:03 +0700

    initial commit

---
+++ dua.js
@@ -0,0 +1,13 @@
+//menginisiasi npm
+/*
+.all
+Type: string[]
+Superhero names in alphabetical order.
+
+.random()
+Type: Function
+Random superhero name
+*/
+const superheroes = require('superheroes');
+
+console.log(superheroes.random());
---
+++ satu.js
@@ -0,0 +1,42 @@
+/*
+console.log("ini contoh untuk mengeluarkan text");
+console.log (1+1);
+*/
+
+//fungsi di javascript
+/*
+function tambah(a,b) {
+    return a+b;
+}*/
+
+//function dengan variabel
+
+function tambah(a,b) {
+    var hasil;
+    hasil = a + b;
+    return hasil;
+}
+
+console.log(tambah(30,90));
+
+//deklarasi variabel let dan var
+/*
+-var var tidak dapat diakses diluar function
+console.log(hasil);
+-let dapat diakses diluar function
+*/
+
+//output
+//console.log(hasil);
+
+
+//constanta, variabel yg tidak dapat diubah
+/*
+const satu=1;
+let helloworld = "Hello World";
+console.log(helloworld);
+helloworld="Mengubah Hello World";
+console.log(helloworld);
+*/
+
+
